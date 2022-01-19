2e8ed83105782cb6ed644a183debe5bea63d1337
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-19 00:44:07 -0600

    mvp done

--- src/reducers/favReducer.js
+++ src/reducers/favReducer.js
@@ -1,3 +1,5 @@
+import {TOGGLE_FAVORITES, ADD_FAVORITE, REMOVE_FAVORITE} from './../actions/favoritesActions';
+
 const initialState = {
   favorites: [{title: 'movie', id: 1}],
   displayFavorites: true
@@ -5,6 +7,24 @@ const initialState = {
 
 const favReducer = (state = initialState, action) => {
   switch(action.type) {
+    case(TOGGLE_FAVORITES):{
+      return{
+        ...state,
+        displayFavorites: !state.displayFavorites
+      }
+    }
+    case(ADD_FAVORITE):{
+      return{
+        ...state,
+        favorites: [...state.favorites, action.payload] ///need to fix key later
+      }
+    }
+    case(REMOVE_FAVORITE):{
+      return{
+        ...state,
+        favorites: state.favorites.filter(item=>(item.id !== action.payload))
+      }
+    }
     default:
       return (state);
   }
